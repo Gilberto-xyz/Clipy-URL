6dea423738205c55a22d1759a5613ce5a4d485c6
Gilberto.xyz <gilberto.nava.marcos@outlook.com>
2023-06-12 04:07:08 -0600

    Version 2 - sql launch

--- Main.py
+++ Main.py
@@ -1,11 +1,33 @@
 import random
 import string
+import sqlalchemy
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
 
 from flask import Flask, render_template, request, redirect
 
-app = Flask(__name__)
+# Define la base de datos
+Base = sqlalchemy.orm.declarative_base()
 
-urls_recortadas = {}
+# Define la clase que representa la tabla
+class Url(Base):
+    __tablename__ = 'urls'
+    id_link = Column(Integer, primary_key=True)
+    url_original = Column(String(255), nullable=False)
+    url_recortada = Column(String(255), nullable=False)
+
+# Crea una instancia del motor de la base de datos
+engine = create_engine('mysql+mysqlconnector://root@localhost:3306/db_url')
+
+# Crea todas las tablas definidas en la base de datos
+Base.metadata.create_all(engine)
+
+# Crea una sesión de SQLAlchemy
+Session = sessionmaker(bind=engine)
+session = Session()
+
+app = Flask(__name__)
 
 def generar_url(length=6):
     caracteres = string.ascii_letters + string.digits
@@ -14,23 +36,37 @@ def generar_url(length=6):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    url_recortada = None
+    url_original = None
     if request.method == 'POST':
         url_original = request.form.get('url_original')
         url_recortada = generar_url()
-        while url_recortada in urls_recortadas:
+        while session.query(Url).filter_by(url_recortada=url_recortada).first() is not None:
             url_recortada = generar_url()
 
-        urls_recortadas[url_recortada] = url_original
-        return render_template('index.html', url_recortada=request.url_root + url_recortada, url_original=url_original)
-    return render_template('index.html')
+        url = Url(url_original=url_original, url_recortada=url_recortada)
+
+        session.add(url)
+        session.commit()
+
+        url_recortada = request.url_root + url_recortada
+        url_original = url_original
+
+    return render_template('index.html', url_recortada=url_recortada, url_original=url_original)
 
 @app.route('/<url_recortada>')
 def redireccionar_url(url_recortada): 
-    url_original = urls_recortadas.get(url_recortada)
-    if url_original:
-        return redirect(url_original)
+    url = session.query(Url).filter_by(url_recortada=url_recortada).first()
+
+    if url:
+        return redirect(url.url_original)
     else:
         return "URL no encontrada", 404
+    
+@app.route('/urls')
+def mostrar_urls():
+    urls = session.query(Url).all()
+    return render_template('urls.html', urls=urls)
 
 if __name__ == '__main__':
     app.run(debug=True)
